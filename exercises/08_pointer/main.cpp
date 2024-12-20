cceec347d2c3f42b11458e24e875c1301f2bba5c
OLd-People <1471913775@qq.com>
2024-12-20 23:55:44 +0800

    good pm

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -3,27 +3,13 @@
 // READ: 数组向指针退化 <https://zh.cppreference.com/w/cpp/language/array#%E6%95%B0%E7%BB%84%E5%88%B0%E6%8C%87%E9%92%88%E7%9A%84%E9%80%80%E5%8C%96>
 bool is_fibonacci(int *ptr, int len, int stride) {
     ASSERT(len >= 3, "`len` should be at least 3");
-    int *start_ptr = ptr; // 初始指向数组的起始位置
-
-    // 找到斐波那契数列的起始点，即第一个非零元素
-    while (start_ptr > ptr && *start_ptr == 0) {
-        start_ptr--;
-    }
-
-    // 检查是否找到了斐波那契数列的起始点
-    if (start_ptr == ptr) {
-        return false; // 如果整个数组都是0，那么它不是一个斐波那契数列
-    }
-
-    // 从找到的起始点开始检查斐波那契数列
-    for (int i = 0; i < len - 2; i++) {
-        int actual_index_i = start_ptr - ptr + i * stride;
-        int actual_index_i_plus_1 = start_ptr - ptr + (i + 1) * stride;
-        int actual_index_i_plus_2 = start_ptr - ptr + (i + 2) * stride;
-
-        if (ptr[actual_index_i_plus_2] != ptr[actual_index_i] + ptr[actual_index_i_plus_1]) {
+    // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
+    int i = 0;
+    for (int count = 2; count < len; ++count){
+        if (ptr[i + 2 * stride] != ptr[i] + ptr[i + stride]){
             return false;
         }
+        i += stride;
     }
     return true;
 }
@@ -41,7 +27,6 @@ int main(int argc, char **argv) {
     ASSERT(!is_fibonacci(arr2    , sizeof(arr2) / sizeof(*arr2)    , 1),         "arr2 is not Fibonacci");
     ASSERT( is_fibonacci(arr2 + 2, 10                              , 2), "part of arr2 is Fibonacci"    );
     ASSERT( is_fibonacci(arr2 + 3,  9                              , 2), "part of arr2 is Fibonacci"    );
-    
     ASSERT(!is_fibonacci(arr2 + 3, 10                              , 2), "guard check"                  );
     ASSERT(!is_fibonacci(arr2 + 1, 10                              , 2), "guard check"                  );
     // clang-format on
