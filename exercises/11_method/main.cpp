5b60b945add17fbfc6a1b24374ecbd4ae9aeb9d9
OLd-People <1471913775@qq.com>
2024-12-20 20:09:39 +0800

    fix new file

--- exercises/11_method/main.cpp
+++ exercises/11_method/main.cpp
@@ -3,12 +3,21 @@
 struct Fibonacci {
     unsigned long long cache[128];
     int cached;
+    Fibonacci():cached(2)
+    {
+        cache[0]=0;
+        cache[1]=1;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     unsigned long long get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        if (i > cached) {
+            for (; cached <= i; ++cached) {
+                cache[cached] = cache[cached - 1] + cache[cached - 2];
+            }
         }
+        
+        
         return cache[i];
     }
 };
@@ -16,7 +25,8 @@ struct Fibonacci {
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
     Fibonacci fib;
-    ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
+    ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
+    
     return 0;
 }
--- exercises/13_class/main.cpp
+++ exercises/13_class/main.cpp
@@ -14,12 +14,25 @@ class Fibonacci {
 
 public:
     // TODO: 实现构造器
-    // Fibonacci()
+    Fibonacci():cached(0)
+    {
+        cache[0]=0;
+        if(sizeof(cache)/sizeof(cache[0])>1)
+        {
+            cache[1]=1;
+        }
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        for (; cached<=i; ++cached) {
+            if(cached>=2){
+                cache[cached] = cache[cached - 1] + cache[cached - 2];
+            }else if(cached == 1){
+                cache[cached] = 1;
+            }
+            
+            
         }
         return cache[i];
     }
@@ -29,7 +42,8 @@ int main(int argc, char **argv) {
     // 现在类型拥有无参构造器，声明时会直接调用。
     // 这个写法不再是未定义行为了。
     Fibonacci fib;
-    ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
+    ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
+    
     return 0;
 }
